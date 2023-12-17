811b47114f400bdf9316a6e26924137c49c79194
Jakub Mieczkowski <jakubmieczkowski01@gmail.com>
2023-12-17 20:02:34 +0100

    Job number is now saved in list

--- main.py
+++ main.py
@@ -3,53 +3,88 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+from datetime import datetime
 import re
+
+data_list = {}
+
+def save_to_list(today, all_offers, junior_offers):
+    # Sprawdź, czy mamy już dwie oferty dla dzisiejszej daty
+    if today in data_list and len(data_list[today]) >= 2:
+        print(f'Two duplicates already exist for {today}. Skipping...')
+        return
+
+    # Dodaj oferty do słownika
+    data_list.setdefault(today, []).extend([all_offers, junior_offers])
+
+    print(f'Data for {today} was added to the list')
 
 def job_counter(url):
     try:
-        #inicalize web
+        # Inicjalizacja przeglądarki
         driver = webdriver.Chrome()
 
-        #open page
+        # Otwarcie strony
         driver.get(url)
 
-        #wait for content to loadd 
+        # Oczekiwanie na załadowanie zawartości
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.CLASS_NAME, 'MuiTab-iconWrapper'))
         )
-        #download source code of this page
+        # Pobranie źródła strony
         page_source = driver.page_source
 
         soup = BeautifulSoup(page_source, 'html.parser')
 
-        #find element you want to find
+        # Znalezienie elementu, który chcesz znaleźć
         jobs_num_element = soup.find('span', {'class': 'MuiTab-iconWrapper'})
 
-        #check if element was found
+        # Sprawdzenie, czy element został znaleziony
         if jobs_num_element:
             number_match = re.search(r'\d+', jobs_num_element.text.strip())
-            
+
             if number_match:
                 number = int(number_match.group())
                 return number
             else:
-                print("Nie można znaleźć cyfr w tekście.")
+                print("Can't find numbers in text.")
                 return None
         else:
-            print("Nie można znaleźć elementu z liczbą.")
+            print("Can't find element with number.")
             return None
     except Exception as e:
-        print(f"Wystąpił błąd: {e}")
+        print(f"Error occurred: {e}")
         return None
     finally:
-        #cloce webpage
+        # Zamknięcie przeglądarki
         driver.quit()
 
+# Przykładowe użycie
+url_all = 'https://justjoin.it/all-locations/python'
+url_junior = 'https://justjoin.it/all-locations/python/experience-level_junior'
 
-url = 'https://justjoin.it/all-locations/python'
-result = job_counter(url)
+result_all = job_counter(url_all)
+result_junior = job_counter(url_junior)
 
-if result is not None:
-    print(f"Number of job offers is {url}: {result}")
+if result_all is not None:
+    print(f"Number of job offers is {url_all}: {result_all}")
 else:
-    print("I can't find any numbers sorry :(")
+    print(f"I can't find any numbers for {url_all} sorry :(")
+
+if result_junior is not None:
+    print(f"Number of job offers is {url_junior}: {result_junior}")
+else:
+    print(f"I can't find any numbers for {url_junior} sorry :(")
+
+# Zapis do słownika data_list
+today = datetime.today().strftime('%Y-%m-%d')
+save_to_list(today, result_all, result_junior)
+
+# Wyświetlenie zawartości słownika data_list
+print("Contents of data_list:")
+for date, offers in data_list.items():
+    print(f"{date}: {offers}")
+
+
+#TO DO 
+#DODAJ OPCJE KTORA BEDZIE WYSWIETLALA DZIENNA ZMIANE LICZBY OFERT, NP +2 ALBO -10 WZGLEDEM DNIA POPRZEDNIEGO 
